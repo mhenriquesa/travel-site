7c052923215824abb3628a186deddf52fd5b0511
Henrique <henriqueator@gmail.com>
2019-03-19 03:48:31 -0300

    Scroll Effects in the features section and testimonials add using JS; Deixando o Script de scroll escalavel

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -16,17 +16,21 @@ class MobileMenu {
 		É dificil de admistrar um grande app dessa forma
 	*/ 
 	constructor() {
+		/*a) Seleção de elementos do DOM*/
 		this.siteHeader = $(".site-header"); 
 		this.menuIcon = $(".site-header__menu-icon");
 		this.menuContent = $(".site-header__menu-content");
+		
 		this.events();
 	}
 
 	events() {
+		/* b) Event Handling */
 		this.menuIcon.click(this.toggleTheMenu.bind(this));
 	}
 
 	toggleTheMenu() {
+		/*c) Defining Functionality*/
 		this.menuContent.toggleClass("site-header__menu-content--is-visible");
 		this.siteHeader.toggleClass("site-header--is-expanded");
 		this.menuIcon.toggleClass("site-header__menu-icon--close-x");
